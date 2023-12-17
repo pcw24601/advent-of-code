cab813d45da85d97da33f4323e5a0e890e74aed5
Paul Wilson <paul.wilson@stormharvester.com>
2023-12-17 13:00:55 +0000

    (WIP) Add day 17 part 1, works on test project, too slow on full problem

---
+++ 17/aoc17a.py
@@ -0,0 +1,117 @@
+import copy
+
+fname = r'17/input.txt'
+# fname = r'17/test.txt'
+
+reverse_direction = dict(l='r', r='l', u='d', d='u')
+
+class Tile():
+    def __init__(self, line_num, col_num, heat_loss):
+        self.line_num = line_num
+        self.col_num = col_num
+        self.heat_loss = heat_loss
+
+
+class Route():
+    def __init__(self, initial_heat_loss):
+        self.path = ''
+        self.line_num = 0
+        self.col_num = 0
+        self._heat_loss = -initial_heat_loss
+
+    def move(self, direction):
+        if direction == 'r':
+            self.col_num += 1
+        elif direction == 'l':
+            self.col_num -= 1
+        elif direction == 'u':
+            self.line_num -= 1
+        elif direction == 'd':
+            self.line_num += 1
+        else:
+            print(f'Invalid direction: {direction}')
+            raise ValueError
+        self.path += direction
+        return self
+    
+
+    @property
+    def position(self):
+        return self.line_num, self.col_num
+    
+
+    @property
+    def recent_path(self):
+        return self.path[-3:]
+    
+
+    @property
+    def heat_loss(self):
+        return self._heat_loss
+    
+
+    @heat_loss.setter
+    def heat_loss(self, val):
+        self._heat_loss = val
+
+
+with open(fname, 'r') as fp:
+    lines = fp.readlines()
+
+tiles_dict = {}
+for line_num, ln in enumerate(lines):
+    for col_number, heat_loss in enumerate(ln.strip()):
+        tiles_dict[(line_num, col_number)] = Tile(line_num, col_number, int(heat_loss))
+last_line = line_num
+last_col = col_number  
+
+list_of_routes = [Route(tiles_dict[(0,0)].heat_loss)]
+total_heat_loss = set()
+moves_dict = {}
+
+while True:
+    new_routes = []
+    for route in list_of_routes:
+
+        # Check on a tile
+        this_tile = tiles_dict.get(route.position, None)
+        if this_tile is None:
+            continue
+        
+        # Add heat_loss
+        route.heat_loss += this_tile.heat_loss
+        # Check if on end position
+        if route.position == (last_line, last_col):
+            total_heat_loss.add(route.heat_loss)
+            continue
+        # Add to moves set--check if we're on a loop AND have a higher heat loss
+        route_loop_check = (route.position, route.recent_path)
+        if route_loop_check in moves_dict:
+            if moves_dict[route_loop_check] < route.heat_loss:
+                continue
+        moves_dict[route_loop_check] = route.heat_loss
+
+        # Generate new positions
+        possible_moves = set('rlud')
+        recent_path = route.recent_path
+        if recent_path:
+            possible_moves.discard(reverse_direction[recent_path[-1]])  # cannot double back
+        if len(recent_path) == 3:
+            # check for going for three steps in a row
+            if len(set(recent_path)) == 1:
+                possible_moves.discard(recent_path[-1])
+        for direction in possible_moves:
+            new_routes.append(copy.deepcopy(route).move(direction))
+    
+    if len(total_heat_loss) > 0:
+        # have found one route. If other routes are longer, discard
+        new_routes = [this_route for this_route in new_routes if this_route.heat_loss < min(total_heat_loss)]
+    new_routes.sort(key=lambda route: route.heat_loss)
+    list_of_routes = new_routes
+    if not list_of_routes:
+        break
+
+print(min(total_heat_loss))
+
+
+
