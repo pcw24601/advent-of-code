a6e99ac54390b84119993640809fc1bcba1ce684
Paul Wilson <paul.wilson@stormharvester.com>
2023-12-19 22:35:57 +0000

    Add working version of day 17 part 1.

--- 17/aoc17a.py
+++ 17/aoc17a.py
@@ -3,126 +3,86 @@ import copy
 fname = r'17/input.txt'
 # fname = r'17/test.txt'
 
-reverse_direction = dict(l='r', r='l', u='d', d='u')
-
-class Tile():
-    def __init__(self, line_num, col_num, heat_loss):
-        self.line_num = line_num
-        self.col_num = col_num
-        self.heat_loss = heat_loss
+reverse_direction = dict(l='r', r='l', u='d', d='u', x='')  # 'x' is dummy variable for first point
 
 
-class Route():
-    def __init__(self, initial_heat_loss):
-        self.path = ''
-        self.line_num = 0
-        self.col_num = 0
-        self._heat_loss = -initial_heat_loss
-
-    def move(self, direction):
-        if direction == 'r':
-            self.col_num += 1
-        elif direction == 'l':
-            self.col_num -= 1
-        elif direction == 'u':
-            self.line_num -= 1
-        elif direction == 'd':
-            self.line_num += 1
-        else:
-            print(f'Invalid direction: {direction}')
-            raise ValueError
-        self.path += direction
-        return self
-    
-
-    @property
-    def position(self):
-        return self.line_num, self.col_num
-    
-
-    @property
-    def recent_path(self):
-        return self.path[-3:]
-    
-
-    @property
-    def heat_loss(self):
-        return self._heat_loss
-    
-
-    @heat_loss.setter
-    def heat_loss(self, val):
-        self._heat_loss = val
-
-
+print('Loading heat map')
 with open(fname, 'r') as fp:
     lines = fp.readlines()
-
-print('Loading route')
-tiles_dict = {}
+# heat_map[(line_num, col_num)] = {
+#   heat_loss: 3,  # heat loss on this square
+#   heat_loss_recent_routes:
+#       {'l': 12, 'll': 9, 'r': 14, 'u': 6, 'uu': 5, 'uuu': 4}
+# }    
+heat_map = {}
 for line_num, ln in enumerate(lines):
     for col_number, heat_loss in enumerate(ln.strip()):
-        tiles_dict[(line_num, col_number)] = Tile(line_num, col_number, int(heat_loss))
+        heat_map[(line_num, col_number)] = int(heat_loss)
 last_line = line_num
-last_col = col_number  
+last_col = col_number 
+solution_upper_bound = last_line * last_col * 9  # do all moves at cost 9
 
-list_of_routes = [Route(tiles_dict[(0,0)].heat_loss)]
-# total_heat_loss = set()
-moves_dict = {}
-print('Starting path search')
-debug_loop_number = 0
 
+# route_set = [
+#   (heat_loss_this_route, recent_route, line_num, col_num), ...  # e.g. (14, 'dd'), this will sort 'd' before 'dd' if the heat is the same
+# ]
+route_set = {(-heat_map[(0,0)], 'x', 0, 0)}  # we don't count first square heat, so subtract and it will be added on again,
+# initial direction 'x' as not in 'lrud' to avoid restriction on rolling backwards
+processed_routes = {}  # {(recent_route, line, col): min_heat_so_far, ...} e.g. {('rr', 3, 5): 12}
+
+loop_num = 0
 while True:
-    debug_loop_number += 1
-    if not list_of_routes:
-        print('All routes checked')
-        break
-    route = list_of_routes.pop(0)
-    if not debug_loop_number % 1000:
-        # print update info
-        print(f'Loop: {debug_loop_number}, routes on list: {len(list_of_routes)}, heat_loss = {route.heat_loss}')
-
-    # Check on a tile
-    this_tile = tiles_dict.get(route.position, None)
-    if this_tile is None:
-        continue
+    loop_num += 1
+    # pop shortest tile from route_list
+    route_set.remove(route_tuple:=min(route_set))
+    heat_loss_this_route, recent_route, line_num, col_num = route_tuple
     
-    # Add heat_loss
-    route.heat_loss += this_tile.heat_loss
-    # Check if on end position
-    if route.position == (last_line, last_col):
-        total_heat_loss = route.heat_loss
+    # chk--not in processed routes
+    saved_tuple = (recent_route, line_num, col_num)
+    if saved_tuple in processed_routes:
+        continue  # we've been here before with at least as low a heat score
+    
+    if (this_heat_loss:=heat_map.get((line_num, col_num), None)) is None:
+        continue  # location is off map
+    heat_loss_this_route += this_heat_loss
+ 
+    #   chk--(line, col) == (last_line, last_col)? return heat_loss_recent_route + current_heat_loss
+    if (line_num, col_num) == (last_line, last_col):
+        print(heat_loss_this_route)
         break
-    # Add to moves set--check if we're on a loop AND have a higher heat loss
-    route_loop_check = (route.position, route.recent_path)
-    if route_loop_check in moves_dict:
-        if moves_dict[route_loop_check] < route.heat_loss:
+
+    # chk--no shorter route in processed_routes (for shorter versions of recent_route)
+    if len(recent_route) == 3:
+        if processed_routes.get((recent_route[-2:], line_num, col_num), solution_upper_bound) <= heat_loss_this_route:
             continue
-    moves_dict[route_loop_check] = route.heat_loss
+    if len(recent_route) >= 2:
+        if processed_routes.get((recent_route[-1:], line_num, col_num), solution_upper_bound) <= heat_loss_this_route:
+            continue
+    
+    # Add to processed routes
+    processed_routes[saved_tuple] = heat_loss_this_route
 
-    # Generate new positions
-    possible_moves = set('rlud')
-    recent_path = route.recent_path
-    if recent_path:
-        possible_moves.discard(reverse_direction[recent_path[-1]])  # cannot double back
-    if len(recent_path) == 3:
-        # check for going for three steps in a row
-        if len(set(recent_path)) == 1:
-            possible_moves.discard(recent_path[-1])
-    for direction in possible_moves:
-        list_of_routes.append(copy.deepcopy(route).move(direction))
+    # generate and check adjoining nodes (line, col, recent_route)
+    next_dirs = set('rdlu')
+    next_dirs.discard(reverse_direction[recent_route[-1]])  # can't go backwards
+    if len(recent_route) == 3:
+        next_dirs.discard(recent_route[-1])  # can't do more than 3 steps in a row
+    for next_dir in next_dirs:
+        new_line_num = line_num
+        new_col_num = col_num
 
-    list_of_routes.sort(key=lambda route: route.heat_loss)
+        if next_dir == recent_route[-1]:
+            new_recent_route = recent_route + next_dir
+        else:
+            new_recent_route = next_dir
 
-# if len(total_heat_loss) > 0:
-#     # have found one route. If other routes are longer, discard
-#     new_routes = [this_route for this_route in new_routes if this_route.heat_loss < min(total_heat_loss)]
-# new_routes.sort(key=lambda route: route.heat_loss)
-# list_of_routes = new_routes
-# if not list_of_routes:
-#     break
+        if next_dir == 'r':
+            new_col_num += 1
+        elif next_dir == 'l':
+            new_col_num -= 1
+        elif next_dir == 'u':
+            new_line_num -= 1
+        elif next_dir == 'd':
+            new_line_num += 1
 
-print(total_heat_loss)
-
-
-
+        route_set.add((heat_loss_this_route, new_recent_route, new_line_num, new_col_num))
