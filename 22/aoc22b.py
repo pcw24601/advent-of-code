ba8fc853ca520f6b608427f0a1423f11c7a1467b
Paul Wilson <paul.wilson@stormharvester.com>
2024-01-07 20:16:36 +0000

    Add day 22 part 2

---
+++ 22/aoc22b.py
@@ -0,0 +1,83 @@
+from pprint import pprint
+import numpy as np
+from copy import deepcopy
+
+
+fname = r'22/input.txt'
+# fname = r'22/test.txt'
+
+class Brick:
+    def __init__(self, x1, y1, z1, x2, y2, z2, name):
+        self.x1 = x1
+        self.y1 = y1
+        self.z1 = z1
+        self.x2 = x2
+        self.y2 = y2
+        self.z2 = z2
+        self.name = name + 1
+    
+    def __repr__(self):
+        return f'Brick(x1={self.x1}, y1={self.y1}, z1={self.z1}, x2={self.x2}, y2={self.y2}, z2={self.z2}, name={self.name})'
+    
+    def __lt__(self, other):
+        return self.z1 < other.z1
+
+    def add_to_grid(self, grid):
+        # drop block down while slot on row below is empty
+        while np.count_nonzero(grid[self.x1: self.x2+1, self.y1: self.y2+1, self.z1-1: self.z2]) == 0:
+            self.z1 -= 1
+            self.z2 -= 1
+        grid[self.x1: self.x2+1, self.y1: self.y2+1, self.z1: self.z2+1] = self.name
+
+    def rests_on_bricks(self, grid):
+        """Return a list of brick numbers that this brick rests on"""
+        bricks_set = set(np.unique(grid[self.x1: self.x2+1, self.y1: self.y2+1, self.z1-1]))
+        bricks_set.discard(0)
+        # bricks_set.discard(-1)
+        return bricks_set
+    
+    def delete_brick(self, grid):
+        grid[np.where(grid==self.name)] = 0
+
+
+
+with open(fname, 'r') as fp:
+    lines = fp.readlines()
+
+max_x = 0
+max_y = 0
+max_z = 0
+bricks = []
+for index_, ln in enumerate(lines):
+    coords = ln.replace('~', ',').split(',')
+    coords = (int(coord) for coord in coords)
+    this_brick = Brick(*coords, index_)
+    bricks.append(this_brick)
+    max_x = max(max_x, this_brick.x1, this_brick.x2)
+    max_y = max(max_y, this_brick.y1, this_brick.y2)
+    max_z = max(max_z, this_brick.z1, this_brick.z2)
+
+bricks.sort()
+# pprint(bricks)
+
+grid = np.zeros((max_x+1, max_y+1, max_z+1), int)
+# set ground as -1
+grid[:,:,0] = -1
+for brick in bricks:
+    brick.add_to_grid(grid)
+
+# delete each brick, then count how many more would be deleted if it disintegrated
+grid_copy = deepcopy(grid)
+number_deleted = 0
+for index_, brick in enumerate(bricks):
+    grid = deepcopy(grid_copy)
+    brick.delete_brick(grid)
+    for test_brick in bricks[index_+1:]:
+        if not test_brick.rests_on_bricks(grid):
+            # test_brick will also disintegrate
+            number_deleted += 1
+            test_brick.delete_brick(grid)
+
+print(number_deleted)
+
+
